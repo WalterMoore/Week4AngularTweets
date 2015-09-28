4e8edbe66b0c5dc5f5032124c6426dcd742138d7
bamaswampmonster <bamaswampmonster@bamaswampmonsters-MacBook-Pro.local>
2015-09-28 14:26:40 -0500

    more work

--- Template 2/client/js/main.js
+++ Template 2/client/js/main.js
@@ -1,8 +1,52 @@
-angular.module('myApp', [])
-	.controller('MyController', ['', function(){
+angular.module('myApp', ['ngRoute'])
+	.controller('MyController', ['ngView', function(){
 
 	}])
-	.config(['', function(){
+	
+	.controller('WelcomeController', function () {
+		
+	})
+	
+	.controller('TweetsController', function() {
+		
+	})
+	
+	.config(function($routeProvider){
+		$routeProvider
+			.when('/', {
+				controller: "WelcomeController",
+				templateUrl: "views/welcome.html"
+			})
+			.when('', {
+				controller: "TweetsController",
+				templateUrl: "veiws/tweets.html"
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
+		
+
+});
+
+/*
+var app = angular.module('GalleryApp', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      controller: "HomeController",
+      templateUrl: "views/home.html"
+    })
+    .when('/photos/:id', {
+      controller: 'PhotoController',
+      templateUrl: 'views/photo.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});*/
 
 
-}]);
+ /*app.controller('StoreController', function(){
+    this.product = gem;
+  });*/
